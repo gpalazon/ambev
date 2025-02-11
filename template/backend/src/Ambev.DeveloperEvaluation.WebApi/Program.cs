4e920ee02395eea5333f87c9b9cb92f547b9b7be
gpalazon <gustavo.palazon@gmail.com>
2025-02-10 23:21:03 -0300

    chore: initial commit

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Program.cs
@@ -0,0 +1,111 @@
+﻿using Ambev.DeveloperEvaluation.Application;
+using Ambev.DeveloperEvaluation.Common.HealthChecks;
+using Ambev.DeveloperEvaluation.Common.Logging;
+using Ambev.DeveloperEvaluation.Common.Security;
+using Ambev.DeveloperEvaluation.Common.Validation;
+using Ambev.DeveloperEvaluation.IoC;
+using Ambev.DeveloperEvaluation.ORM;
+using Ambev.DeveloperEvaluation.WebApi.Middleware;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+namespace Ambev.DeveloperEvaluation.WebApi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Log.Information("Starting web application");
+
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            builder.AddDefaultLogging();
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.AddBasicHealthChecks();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<DefaultContext>(options =>
+                options.UseNpgsql(
+                    builder.Configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly("Ambev.DeveloperEvaluation.ORM")
+                )
+            );
+
+            builder.Services.AddJwtAuthentication(builder.Configuration);
+
+            builder.RegisterDependencies();
+
+            builder.Services.AddAutoMapper(typeof(Program).Assembly, typeof(ApplicationLayer).Assembly);
+
+            builder.Services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssemblies(
+                    typeof(ApplicationLayer).Assembly,
+                    typeof(Program).Assembly
+                );
+            });
+
+            builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            var app = builder.Build();
+            app.UseMiddleware<ValidationExceptionMiddleware>();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseBasicHealthChecks();
+
+            app.MapControllers();
+
+           
+            ApplyMigrations(app);
+
+
+            app.Run();
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Application terminated unexpectedly");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+
+    /// <summary>
+    /// Automatically applies migrations when starting the application.
+    /// </summary>
+    private static void ApplyMigrations(WebApplication app)
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<DefaultContext>();
+
+            try
+            {
+                Log.Information("Applying database migrations...");
+                dbContext.Database.Migrate();
+                Log.Information("Database migrations applied successfully.");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "An error occurred while applying database migrations.");
+                throw;
+            }
+        }
+    }
+}
